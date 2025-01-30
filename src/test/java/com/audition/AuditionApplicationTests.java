76c969bf19a98a0a4928b60090657b3b740d6195
yasiru samarasekara <ysamarasekara@isa.ae>
2025-01-31 09:14:03 +1100

    AuditionApplicationTests unit test refines

--- src/test/java/com/audition/AuditionApplicationTests.java
+++ src/test/java/com/audition/AuditionApplicationTests.java
@@ -2,12 +2,14 @@ package com.audition;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
+@Getter
 class AuditionApplicationTests {
 
     @Autowired
